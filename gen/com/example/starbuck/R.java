9d23f10deb2e519434c66ad65052c299b80e7c62
Endi <e.desyuntiadi@gmail.com>
2014-06-12 15:43:23 +0700

    layout

--- gen/com/example/starbuck/R.java
+++ gen/com/example/starbuck/R.java
@@ -22,16 +22,27 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int sierra_icon=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int pager=0x7f080000;
-        public static final int section_label=0x7f080001;
+        public static final int Activation=0x7f080000;
+        public static final int CreatePin=0x7f080004;
+        public static final int Login=0x7f080008;
+        public static final int action_settings=0x7f08000a;
+        public static final int btnActivate=0x7f080006;
+        public static final int btnlogin=0x7f080003;
+        public static final int imageView1=0x7f080007;
+        public static final int password=0x7f080002;
+        public static final int pin=0x7f080005;
+        public static final int pinLogin=0x7f080009;
+        public static final int username=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
+        public static final int activation=0x7f030000;
+        public static final int activation_pin=0x7f030001;
+        public static final int login=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
